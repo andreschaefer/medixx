3e83ed0405c77a5591fd4db1f61f1a2883db452d
Andre Schaefer <andre.schaefer@namics.com>
2014-12-23 23:23:04 +0100

    progress

--- app/js/controllers/detail.js
+++ app/js/controllers/detail.js
@@ -4,6 +4,12 @@ angular.module('Medixx').controller('DetailCtrl', ['$scope', '$routeParams', '$m
     function ($scope, $routeParams, $medics) {
         $scope.medics = $medics.get();
         $scope.medicId = $routeParams.medicId;
+        $scope.medic = null;
+        angular.forEach($scope.medics.stocks, function (value) {
+            if (value.id == $scope.medicId) {
+                $scope.medic = value;
+            }
+        })
         $scope.add = function (amount) {
             this.medic.stock += amount;
             $medics.save();
