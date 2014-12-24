4f52e3ac76f42abd5bf9a8f7e0c7b3f08b1037c3
Andre Schaefer <andre.schaefer@namics.com>
2014-12-24 13:57:55 +0100

    list coloring

--- src/main/webapp/js/controllers/list.js
+++ src/main/webapp/js/controllers/list.js
@@ -1,8 +1,11 @@
 'use strict';
-angular.module('Medixx').controller('ListCtrl', ['$scope', '$log', '$routeParams', '$medics',
+angular.module('Medixx').controller('ListCtrl', ['$scope', '$log', '$routeParams', '$medics','$location',
 
-    function ($scope, $log, $routeParams, $medics) {
+    function ($scope, $log, $routeParams, $medics,$location) {
         $scope.medics = $medics.get();
         $scope.orderProp = 'name';
+        $scope.go = function (path) {
+            $location.path(path);
+        };
     }
 ]);
